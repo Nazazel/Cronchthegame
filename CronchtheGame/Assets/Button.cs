83db09a8fc7db479d38aba0fbc53c5c53067f7d0
Ittaimann <ittaiam@gmail.com>
2019-04-13 19:13:49 -0700

    Button script

---
+++ CronchtheGame/Assets/Button.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private bool triggered{get;set;}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player"|| other.tag == "DeadBody")
+        {
+            triggered = true; 
+        }
+    }
+}
