a0c814f128d5cdc5035cdac4782ebfa9628ae825
Mayan Shoshani <mshoshan@uci.edu>
2019-04-14 15:16:10 -0700

    added sound

--- CronchtheGame/Assets/Scripts/LevelSwitch.cs
+++ CronchtheGame/Assets/Scripts/LevelSwitch.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 
 public class LevelSwitch : MonoBehaviour
 {
-
+    public AudioClip victorySound;
+    
+    private AudioSource audio;
+    private float duration;
     public string level;
+            
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+
+    }
+
+    IEnumerator WaitForSound()
+    {
+        yield return new WaitForSeconds(duration);
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
+
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene(level, LoadSceneMode.Single);
+            audio.clip = victorySound;
+            audio.Play();
+            duration = victorySound.length;
+            StartCoroutine(WaitForSound());
+
         }
     }
 
