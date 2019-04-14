0820577d139f15440326b27bed5bea6932870692
Ittaimann <ittaiam@gmail.com>
2019-04-14 14:23:05 -0700

    open door script

---
+++ CronchtheGame/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject[] Buttons;
+    private bool[] allPressed;
+    // Start is called before the first frame  
+    void Start()
+    {
+        allPressed = new bool[Buttons.Length];
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool check = true;
+
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            if(!Buttons[i].GetComponent<Button>().triggered)
+            {
+                check = false;
+                break;
+            }
+        }
+        if(check == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
