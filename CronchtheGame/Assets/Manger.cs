ee688ae8ebb32cbdb3b2db68cd287550e023814d
Ittaimann <ittaiam@gmail.com>
2019-04-14 16:16:13 -0700

    made main menu wip

---
+++ CronchtheGame/Assets/Manger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Manger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void start()
+    {   
+        SceneManager.LoadScene("level_1");
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    public void credits()
+    {
+        
+    }
+}
